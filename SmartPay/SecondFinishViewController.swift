1ea242ea6d7696611b85c0db5fee17ad4dd4d0a3
Salomon <salomonpluviose@yahoo.com>
2016-12-21 20:29:43 -0500

    added a few methods

---
+++ SmartPay/SecondFinishViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondFinishViewController.swift
+//  SmartPay
+//
+//  Created by Salomon Pluviose on 12/21/16.
+//  Copyright © 2016 Smarter Apps. All rights reserved.
+//
+
+import UIKit
+
+class SecondFinishViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
